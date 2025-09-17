4fbfc7b8e7cd44dc90f85271489d47e9c6cb0b35
gdl1014kr <161820700+gdl1014kr@users.noreply.github.com>
2025-09-17 17:57:49 +0900

    Update Demo.m

--- Demo.m
+++ Demo.m
@@ -1,16 +1,16 @@
 %   This script runs the implementation of EMCF, which is borrowed from
 %   BACF.
 
-clear; 
-clc;
-close all;
-setup_paths;
+clear; %  MATLAB 환경의 모든 변수, 명령어 창, 열린 그림 창을 초기화
+clc; %  MATLAB 환경의 모든 변수, 명령어 창, 열린 그림 창을 초기화
+close all; %  MATLAB 환경의 모든 변수, 명령어 창, 열린 그림 창을 초기화
+setup_paths; %  이 추적 알고리즘을 실행하는 데 필요한 모든 함수 파일(run_DSHCF, load_video_info, computePascalScore 등)이 있는 폴더들을 MATLAB의 검색 경로에 추가하는 함수
 
 %   Load video information
 % base_path  = 'F:/tracking/datasets/UAVTrack112';
 
-base_path = './seq';
-video      = 'Yacht2';
+base_path = './seq'; % 데이터셋의 기본 경로를 seq 폴더로 지정
+video      = 'Yacht2'; %
 video_path = [base_path '/' video];
 [seq, ground_truth] = load_video_info(video,base_path,video_path);
 seq.path = video_path;
