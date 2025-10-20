b9e8b73842a81305c3ada50d8dbe19cf479ffa19
gdl1014kr <161820700+gdl1014kr@users.noreply.github.com>
2025-10-20 22:31:21 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,8 +28,8 @@ conda install -y -c conda-forge gcc=8 gxx=8
 echo ""
 echo ""
 echo "****************** Installing pytorch with cuda10 ******************"
-# 수정 3: PyTorch 1.4.0 및 CUDA 10.1 버전 고정
-conda install -y pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
+# conda install -y pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
+conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
 
 echo ""
 echo ""
