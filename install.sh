5d7b007df34e15381fed9ad2d4aaceaefab27546
gdl1014kr <161820700+gdl1014kr@users.noreply.github.com>
2025-10-21 14:51:36 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,8 @@ conda install -y -c conda-forge gcc=8 gxx=8
 echo ""
 echo ""
 echo "****************** Installing pytorch with cuda10 ******************"
-conda install -y pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
+conda install pytorch==1.2.0 torchvision==0.4.0 cudatoolkit=10.0 -c pytorch
+
 
 echo ""
 echo ""
